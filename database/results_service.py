d167602e14cea2adbce549f8f14f32be38319b7a
Kantemir <kan.shandir94@gmail.com>
2026-06-27 20:05:35 +0300

    added first version fix v6

--- database/results_service.py
+++ database/results_service.py
@@ -27,20 +27,37 @@ def update_result(team_id: str, win=0, draw=0, loss=0):
     }).eq("team_id", team_id).execute()
 
 
-def finish_match(match):
+def finish_match(match_id: str):
 
+    match = (
+        supabase.table("matches")
+        .select("*")
+        .eq("id", match_id)
+        .single()
+        .execute()
+    ).data
+
+    if not match:
+        return
 
     a = match["score_team_a"]
     b = match["score_team_b"]
 
+    team_a = match["team_a_id"]
+    team_b = match["team_b_id"]
+
     if a > b:
-        update_result(match["team_a_id"], win=1)
-        update_result(match["team_b_id"], loss=1)
+        update_result(team_a, win=1)
+        update_result(team_b, loss=1)
 
     elif b > a:
-        update_result(match["team_b_id"], win=1)
-        update_result(match["team_a_id"], loss=1)
+        update_result(team_b, win=1)
+        update_result(team_a, loss=1)
 
     else:
-        update_result(match["team_a_id"], draw=1)
-        update_result(match["team_b_id"], draw=1)
+        update_result(team_a, draw=1)
+        update_result(team_b, draw=1)
+
+    supabase.table("matches").update({
+        "status": "finished"
+    }).eq("id", match_id).execute()
